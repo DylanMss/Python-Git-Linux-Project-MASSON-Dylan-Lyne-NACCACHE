eb1b51fb469851dc3e8f4da2380d5f37f65ad021
DylanMss <dylanmasson67@gmail.com>
2025-03-19 14:13:03 +0100

    traduction

--- scraping.sh
+++ scraping.sh
@@ -11,33 +11,32 @@ HTML=$(curl -s -L -A "Mozilla/5.0" "$URL")
 # On supprime les retours à la ligne pour metre tout le HTML sur une ligne
 HTML_ONELINE=$(echo "$HTML" | sed ':a;N;$!ba;s/\n//g')
 
-
 echo "$HTML_ONELINE" > debug.html
-echo "Le contenu HTML a ete sauvegarde dans debug.html"
+echo "The HTML content has been saved in debug.html"
 
 # voir si ca commence bien
 echo "$HTML_ONELINE" | head -n 1 | cut -c1-300
 
 # extraire le prix dans la balise span avec id hprice
-
 PRICE=$(echo "$HTML_ONELINE" | grep -oP '(?<=\$)[0-9,\.]+' | head -n 1)
 
 if [ -z "$PRICE" ]; then
-    echo "Erreur : Prix non trouvé."
+    echo "Error: Price not found."
     exit 1
 fi
 
-echo "Prix actuel du Bitcoin : \$$PRICE"
+echo "Current Bitcoin price: \$$PRICE"
 
 # On supprime la virgule des milliers pour que le CSV ait deux colonnes seulement
 PRICE_CLEAN=$(echo "$PRICE" | tr -d ',')
 
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 CSV_FILE="prices.csv"
+
 # Si le fichier n'existe pas ou est vide, on ajoute l'entête
 if [ ! -f "$CSV_FILE" ] || [ ! -s "$CSV_FILE" ]; then
     echo "timestamp,price" > "$CSV_FILE"
 fi
 
 echo "$TIMESTAMP,$PRICE_CLEAN" >> "$CSV_FILE"
-echo "Donnee sauvegardee dans $CSV_FILE"
+echo "Data saved in $CSV_FILE"
